8606c34ae48f7d0e0d103f59cf2d140944d73590
Kenji <122567871+xKenjiR@users.noreply.github.com>
2025-08-14 16:08:40 +0800

    create header

---
+++ src/components/Header.tsx
@@ -0,0 +1,21 @@
+const Header = () => {
+  return (
+    <div
+      className="
+        fixed
+        w-full lg:w-[90%]
+        p-3 lg:ml-[5%]
+        lg:rounded-b-md
+        text-2xl font-medium
+        text-center
+        bg-white
+        shadow-sm
+        transition-all
+      "
+    >
+      CSS Presentation
+    </div>
+  );
+};
+
+export default Header;
